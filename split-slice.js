36bba057488c400658bb5524874b691afb38981a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-02 18:56:33 +0600

    ad to array destructing part

---
+++ split-slice.js
@@ -0,0 +1,8 @@
+//split
+var ami = 'amar name sani. ami alhamdolillah vallo asi. tomra sobay kemon aso';
+// var split = ami.split('.');
+
+
+//slice
+var slice = name.slice(2, 7);
+console.log(slice);
---
+++ temp.js
@@ -0,0 +1,17 @@
+let number1 = 5;
+let number2 = 7;
+console.log(number1, number2);
+
+//1
+// let temp = number1;
+// number1 = number2;
+// number2 = temp;
+
+
+//2 array Destructing 
+[number1, number2] = [number2, number1];
+console.log(number1, number2);
+
+
+
+
