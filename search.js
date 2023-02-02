f09fecc6edf2041839ea4726ba65086b8aa36bc9
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-02 09:55:33 +0600

    search string item part

---
+++ search.js
@@ -0,0 +1,24 @@
+//includes
+const madrasaName = 'pakunda islamia madrasA'
+// const check = madrasaName.includes('mAdrasa');
+var word = 'mAdrasa';
+let toLowerCase = word.toLowerCase()
+let search = madrasaName.toLowerCase().includes(toLowerCase);
+// console.log(search)
+
+
+
+//startswith
+const ami = 'ami alhamdolillh vallo asi';
+const check = ami.startsWith('ami')
+// console.log(check)
+
+
+
+//endswith
+const bookName = 'bangla arbi gonit somaj';
+const bookSearch = bookName.endsWith('somaj')
+console.log(bookSearch)
+
+
+
